6d70ea6af8f208aa7d343e9d2df49468e2e1004d
yuvrajput12 <110720025+yuvrajput12@users.noreply.github.com>
2024-01-17 13:32:03 +0000

    strings

---
+++ 01_basic/string.js
@@ -0,0 +1,19 @@
+const name = "yuvraj"
+const repoCount = 10
+// console.log(name + repoCount + " value")
+// console.log(`My Name is ${name} and my repo count is ${repoCount}`)
+
+
+
+const newName = new String("Yuvraj") 
+
+// console.log(newName.length)
+// console.log(newName.toUpperCase())
+// console.log(newName.charAt(2))
+// console.log(newName.indexOf('r'))
+
+const newString = newName.substring(0,4)
+console.log(newString)
+const newString1 = newName.slice(-6,3)
+
+console.log(newString1)
