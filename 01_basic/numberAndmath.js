a2832a0fab5171b5d48b21dc1cb601db0be9218d
yuvrajput12 <110720025+yuvrajput12@users.noreply.github.com>
2024-01-23 17:59:23 +0000

    math and Number

---
+++ 01_basic/numberAndmath.js
@@ -0,0 +1,29 @@
+const score = 400
+// console.log(score)
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+
+
+const otherNumber = 121.372
+// console.log(otherNumber.toPrecision(3));
+
+const hundred = 1000000000
+// console.log(hundred.toLocaleString());
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+
+
+//***********************Maths *****************/
+
+
+console.log(Math)
+console.log(Math.abs(-3))
+console.log(Math.round(3.4))
+console.log(Math.ceil(6.001))
+console.log(Math.floor(6.3))
+
+
+
