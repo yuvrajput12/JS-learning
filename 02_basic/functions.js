6aea1f998f95768e6a6efe74079e6014f6d7e924
yuvrajput12 <110720025+yuvrajput12@users.noreply.github.com>
2024-02-07 14:09:12 +0000

    functions

---
+++ 02_basic/functions.js
@@ -0,0 +1,34 @@
+// console.log('Y')
+// console.log('u')
+// console.log('v')
+// console.log('r')
+// console.log('a')
+// console.log('j')
+
+// function myName(){
+// console.log('Y')
+// console.log('u')
+// console.log('v')
+// console.log('r')
+// console.log('a')
+// console.log('j')
+// }
+
+// myName();
+
+// function addnumber(number1,number2){
+//     console.log(number1 + number2);
+// }
+// addnumber(3,4)
+// addnumber(3,"4")
+// addnumber(3,"a")
+// addnumber(3,null)
+
+const addNumber = (number1,number2) => {
+    // let result = number1 + number2;
+    // return result;
+
+    return number1 + number2
+}
+const result = addNumber(3,3)
+console.log("result :" , result)
