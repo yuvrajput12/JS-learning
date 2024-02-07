d071863440ed5ba1b6bbd656c6715e77a4482817
yuvrajput12 <110720025+yuvrajput12@users.noreply.github.com>
2024-02-07 13:56:01 +0000

    destructing

---
+++ 02_basic/object_destructing.js
@@ -0,0 +1,26 @@
+const village = {
+    name : "pargwal",
+    post_office : "pargwal",
+    pin: 181207,
+    district : "jammu",
+}
+// console.log(village.pin)
+
+// destructing
+
+const {pin,name,post_office,district} = village
+
+// console.log(name)
+// console.log(post_office)
+// console.log(pin)
+// console.log(district)
+
+// json
+// {
+//     "name" : "yuvraj",
+//     "last_name" : "singh"
+// }
+// [
+//     {},
+//     {},
+// ]
