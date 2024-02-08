de2e646597b85fb82e736ccca339d70a884012a5
yuvrajput12 <110720025+yuvrajput12@users.noreply.github.com>
2024-02-08 12:41:21 +0000

    new

--- 02_basic/functions.js
+++ 02_basic/functions.js
@@ -24,11 +24,29 @@
 // addnumber(3,"a")
 // addnumber(3,null)
 
-const addNumber = (number1,number2) => {
-    // let result = number1 + number2;
-    // return result;
+// const addNumber = (number1,number2) => {
+//     // let result = number1 + number2;
+//     // return result;
 
-    return number1 + number2
+//     return number1 + number2
+// }
+// const result = addNumber(3,3)
+// console.log("result :" , result)
+
+// rest operator => ...
+// function calculate(...num1){
+//    return num1
+// }
+
+// console.log(calculate(200,3,100,4,4,4,))
+
+
+const user={
+    username:"yuvraj",
+    id : 3
 }
-const result = addNumber(3,3)
-console.log("result :" , result)
+
+function handleobject(anyobject){
+    console.log(`Name is ${anyobject.username} and id is ${anyobject.id}`)
+}
+handleobject(user)
